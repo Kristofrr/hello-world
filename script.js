2f7994330e0eace3ba8b396028384464f4e58c77
thecwadley <51142831+thecwadley@users.noreply.github.com>
2020-08-07 16:56:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
 let video = $("#video");
 console.log(video);
 
-video.width = $(window).innerWidth();
+video.width() = $(window).innerWidth();
 
 $(window).resize(function() {
-   video.width = $(window).innerWidth();
+   video.width() = $(window).innerWidth();

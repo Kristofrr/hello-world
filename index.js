353050b76ceba9c5227adce2a722998809c88b6e
thecwadley <51142831+thecwadley@users.noreply.github.com>
2020-08-07 15:44:28 -0500

    Create index.js
    Changed the video size to fit the screen

---
+++ index.js
@@ -0,0 +1,10 @@
+document
+  .getElementById("video")
+  .setAttribute(
+    "style",
+    "position: absolute; top: 0px; left: 0px; width: " +
+      (window.innerWidth - 2) +
+      "px; height: " +
+      (window.innerHeight - 2) +
+      "px; border: 1px solid #000000"
+  );

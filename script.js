f6ee0ac212bf390dddff7295eac1a1f2480653fa
thecwadley <51142831+thecwadley@users.noreply.github.com>
2020-08-07 16:33:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ $("#video").height() = $("window").innerHeight();
 $("#video").width() = $("window").innerWidth();
 
 $("window").resize(function() {
-    $('div.box').css({'right': parseInt(window.innerWidth/2),
+    $("#video").css({'right': parseInt(window.innerWidth/2),
                       'bottom': parseInt(window.innerHeight/2)
                      });
 });

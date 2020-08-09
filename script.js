2268b150d91cfecb1473b72b29064ab629dfaaa9
thecwadley <51142831+thecwadley@users.noreply.github.com>
2020-08-09 15:23:42 -0500

    Added prompts on first page load

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+if (localStorage.getItem("setup") === null) {
+  let title = prompt("What is the title of the bookmark?", "Gmail");
+  document.getElementById("title").innerHTML = title;
+  let favIcon = prompt("What is the link to the desired favicon?", "https://ssl.gstatic.com/ui/v1/icons/mail/images/favicon5.ico")
+  document.getElementById("favIcon").attributes.href = favIcon;
+  localStorage.setItem("setup", "false");
+} else {
+  document.getElementById("video").attributes.autoplay = "true";
+  document.getElementById("video").attributes.muted = "true";
+}
+
 document
   .getElementById("video")
   .setAttribute(

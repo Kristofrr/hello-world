e8bb64982b182950e050e02734fbf0d4b1eeeb45
thecwadley <51142831+thecwadley@users.noreply.github.com>
2020-08-09 15:29:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 if (localStorage.getItem("setup") === null) {
   let title = prompt("What is the title of the bookmark?", "Gmail");
   document.getElementById("title").innerHTML = title;
+  localStorage.setItem("title", title);
   let favIcon = prompt("What is the link to the desired favicon?", "https://ssl.gstatic.com/ui/v1/icons/mail/images/favicon5.ico")
   document.getElementById("favIcon").attributes.href = favIcon;
+  localStorage.setItem("favIcon", favIcon);
   localStorage.setItem("setup", "false");
 } else {
-  document.getElementById("video").attributes.autoplay = "true";
   document.getElementById("video").attributes.muted = "true";
+  document.getElementById("video").attributes.autoplay = "true";
+  document.getElementById("title").innerHTML = localStorage.getItem("title");
+  document.getElementById("favIcon").attributes.href = localStorage.getItem("favIcon");
 }
 
 document
